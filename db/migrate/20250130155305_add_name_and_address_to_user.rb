9cfb59c54e7f865825c3bfbdd0feecad066ecd43
WildCard-NO <kalex.boe@gmail.com>
2025-01-30 17:08:00 +0100

    Make it possible to add custom tailwind css styling to devise

---
+++ db/migrate/20250130155305_add_name_and_address_to_user.rb
@@ -0,0 +1,10 @@
+class AddNameAndAddressToUser < ActiveRecord::Migration[7.1]
+  def up
+    add_column :users, :name, :string
+    add_column :users, :address, :string
+    end
+    def down
+    remove_column :users, :name
+    remove_column :users, :address
+    end
+end
